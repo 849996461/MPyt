e4967009bee6ec79b157c8ac327cc142f4d2f19b
chenweiheng <849996461@qq.com>
2020-10-27 23:38:36 +0800

    fix

--- iron4/iron4-defines.py
+++ iron4/iron4-defines.py
@@ -13,12 +13,12 @@ fuel_consumption\s+?=\s+?(\d+\.?\d+)--float(g1)*5
 if __name__ == '__main__':
     # 唯一参数
     modPath = r"C:\Users\84999\Documents\Paradox Interactive\Hearts of Iron IV\mod\adorable_heart193"
-
-
     ptns = getPatterns(pts, "--")
+
+    
     for file in listPath(modPath+"/common/units/equipment",".*air.*"):
         ctx = readFile(file)
-        replaceByPatterns(ctx,ptns)
+        ctx = replaceByPatterns(ctx,ptns)
         writeFile(file,ctx)
 
 
---
+++ iron4/iron4-replaceAir.py
@@ -0,0 +1,31 @@
+
+from MPyt.fileT import *
+
+pts = """
+naval_strike_attack\s+?=\s+?(\d+\.?\d+)--float(g1)*10
+air_superiority\s+?=\s+?(\d+\.?\d+)--float(g1)*10
+build_cost_ic\s+?=\s+?(\d+\.?\d+)--float(g1)*10
+manpower\s+?=\s+?(\d+\.?\d+)--float(g1)*5
+air_bombing\s+?=\s+?(\d+\.?\d+)--float(g1)*10
+air_ground_attack\s+?=\s+?(\d+\.?\d+)--float(g1)*10
+fuel_consumption\s+?=\s+?(\d+\.?\d+)--float(g1)*5
+"""
+if __name__ == '__main__':
+    # 唯一参数
+    modPath = r"C:\Users\84999\Documents\Paradox Interactive\Hearts of Iron IV\mod\adorable_heart193"
+    ptns = getPatterns(pts, "--")
+
+    
+    for file in listPath(modPath+"/common/units/equipment",".*air.*"):
+        ctx = readFile(file)
+        ctx = replaceByPatterns(ctx,ptns)
+        writeFile(file,ctx)
+
+
+
+
+
+
+
+
+
