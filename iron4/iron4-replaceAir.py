8fb08eb8bc078d98dedccc071bc2ad7da96f9cbb
chenweiheng <849996461@qq.com>
2020-10-29 00:35:36 +0800

    fix

--- iron4/iron4-replaceAir.py
+++ iron4/iron4-replaceAir.py
@@ -2,13 +2,14 @@
 from MPyt.fileT import *
 import time
 pts = """
-naval_strike_attack\s+?=\s+?(\d+\.?\d+)--float(g1)*10
-air_superiority\s+?=\s+?(\d+\.?\d+)--float(g1)*10
-build_cost_ic\s+?=\s+?(\d+\.?\d+)--float(g1)*10
-manpower\s+?=\s+?(\d+\.?\d+)--float(g1)*5
-air_bombing\s+?=\s+?(\d+\.?\d+)--float(g1)*10
-air_ground_attack\s+?=\s+?(\d+\.?\d+)--float(g1)*10
-fuel_consumption\s+?=\s+?(\d+\.?\d+)--float(g1)*5
+naval_strike_attack\s+?=\s+?(\d+\.?\d*)--float(g1)*10
+air_superiority\s+?=\s+?(\d+\.?\d*)--float(g1)*10
+build_cost_ic\s+?=\s+?(\d+\.?\d*)--float(g1)*10
+manpower\s+?=\s+?(\d+\.?\d*)--float(g1)*5
+air_bombing\s+?=\s+?(\d+\.?\d*)--float(g1)*10
+air_ground_attack\s+?=\s+?(\d+\.?\d*)--float(g1)*10
+fuel_consumption\s+?=\s+?(\d+\.?\d*)--float(g1)*5
+
 """
 if __name__ == '__main__':
     # 唯一参数
@@ -19,7 +20,7 @@ if __name__ == '__main__':
         ctx = readFile(file)
         ctx = replaceByPatterns(ctx,ptns)
         print(ctx)
-        # writeFile(file,ctx)
+        writeFile(file,ctx)
 
 
 
